91bbffbbc6c7c4f65f3c53d2de595dbf36e90d2e
James <jzming.li@gmail.com>
2023-10-27 18:54:36 -0400

    ACT 1

--- Assets/navControl.cs
+++ Assets/navControl.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class navControl : MonoBehaviour
 {
     [SerializeField] GameObject target;
+    [SerializeField] GameObject attackTarget;
     [SerializeField] NavMeshAgent agent;
     bool isWalking = true;
     Animator animator;
@@ -20,19 +21,22 @@ public class navControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.speed = agent.speed/2;
+        animator.speed = agent.speed;
         if (isWalking)
         {
             agent.enabled = true;
             agent.destination = target.transform.position;
         }
-        else agent.enabled = false;
-        
+        else
+        {
+            agent.enabled = false;
+            transform.LookAt(attackTarget.transform.position);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name == "Dragon")
+        if(other.gameObject == target)
         {
             isWalking = false;
             animator.SetTrigger("ATTACK");
@@ -40,7 +44,7 @@ public class navControl : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        if(other.name == "Dragon")
+        if(other.gameObject == target)
         {
             isWalking = true;
             animator.SetTrigger("WALK");
